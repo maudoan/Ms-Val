85547b5167a0f2cdc78a55017eccf444ed48f06e
{maudoan} <{maudoanxb@gmail.com}>
2022-02-13 14:18:23 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// let btn1 = document.getElementById("btnNo");
+function noHover(){
+    document.getElementById("btnNo").value = "CÓ";
+
+}
+function out(){
+    document.getElementById("btnNo").value = "KHÔNG";
+}
+
+function yesClick(){
+  location.href = "love.html";
+}
+function NoClick(){
+    alert(" Đừng dối lòng thế, hãy sống thật với bản thân đi!!!");
+}
